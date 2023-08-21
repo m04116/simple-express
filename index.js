077b00017d3dcc63e151bc2205c948e54e519bb6
kostiantynrodin <m04116@icloud.com>
2023-08-21 18:15:47 +0300

    Add console.log

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+console.log('----', process.env.TTT)
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
