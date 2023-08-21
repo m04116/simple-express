d791623237e8324b9b2e6f9a2b4bc90cbaaa0e86
kostiantynrodin <m04116@icloud.com>
2023-08-21 20:16:40 +0300

    add code from Secret Manager

--- index.js
+++ index.js
@@ -1,13 +1,52 @@
-const express = require('express')
-const app = express()
-const port = 3050
+const express = require("express");
+const app = express();
+const port = 3050;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+// Use this code snippet in your app.
+// If you need more information about configurations or implementing the sample code, visit the AWS docs:
+// https://docs.aws.amazon.com/sdk-for-javascript/v3/developer-guide/getting-started.html
 
-console.log('----', process.env)
+const {
+  SecretsManagerClient,
+  GetSecretValueCommand,
+} = require("@aws-sdk/client-secrets-manager");
+
+const getCredentials = async () => {
+  const secret_name = "qq";
+
+  const client = new SecretsManagerClient({
+    region: "us-east-1",
+  });
+
+  let response;
+
+  try {
+    response = await client.send(
+      new GetSecretValueCommand({
+        SecretId: secret_name,
+        VersionStage: "AWSCURRENT", // VersionStage defaults to AWSCURRENT if unspecified
+      }),
+    );
+  } catch (error) {
+    // For a list of exceptions thrown, see
+    // https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
+    throw error;
+  }
+
+  const secret = response.SecretString;
+  console.log("--secret--", secret);
+};
+
+getCredentials();
+
+// Your code goes here
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+// console.log('----', process.env)
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
